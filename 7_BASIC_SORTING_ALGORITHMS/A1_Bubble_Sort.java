299b953cc7b75446dd11f11a9efbcad42a0f7370
Jayesh <jayesh.jadhav.tech@gmail.com>
2026-08-04 12:38:49 +0530

    UPDATED

--- 7_BASIC_SORTING_ALGORITHMS/A1_Bubble_Sort.java
+++ 7_BASIC_SORTING_ALGORITHMS/A1_Bubble_Sort.java
@@ -1,25 +1,25 @@
-public class A1_Bubble_Sort{
-    public static void BubbleSort(int array[]){
-        for (int turns = 0; turns < array.length-1; turns++) {
-            for (int i = 0; i < array.length-1-turns; i++) {
-                if (array[i] > array[i+1]) {
+public class A1_Bubble_Sort {
+    public static void BubbleSort(int array[]) {
+        for (int turns = 0; turns < array.length - 1; turns++) {
+            for (int i = 0; i < array.length - 1 - turns; i++) {
+                if (array[i] > array[i + 1]) {
                     int temp = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = temp;
-                    
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+
                 }
-                
+
             }
-            
+
         }
-         
 
     }
+
     public static void main(String[] args) {
 
-        int array[] = {2,1,5,4,3};
+        int array[] = { 2, 1, 5, 4, 3 };
 
-        BubbleSort(array);        
+        BubbleSort(array);
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
